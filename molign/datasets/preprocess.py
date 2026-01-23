e338231ca4a3764ae300d0b59456ada72666c1dd
Matthias Welsch <matthias.welsch@univie.ac.at>
2026-01-23 10:08:40 +0100

    Improve preprocessing

--- molign/datasets/preprocess.py
+++ molign/datasets/preprocess.py
@@ -1,8 +1,9 @@
 import pandas as pd
+import torch
 from chembl_structure_pipeline import standardize_mol
 from rdkit import Chem, RDLogger
-from rdkit.Chem import Descriptors
 from rdkit.Chem.SaltRemover import SaltRemover
+from molign.models.model_wrapper import _physico_chemical_raw
 
 
 def standardize_mol_safe(mol):
@@ -12,7 +13,6 @@ def standardize_mol_safe(mol):
     except Exception as e:
         print(e)
         return None
-
 
 def clean(data: pd.DataFrame):
     RDLogger.DisableLog("rdApp.*")
@@ -28,12 +28,22 @@ def clean(data: pd.DataFrame):
     data = data[~data.smiles.isna()]
     data = data[data.smiles != ""]
     data = data[data.smiles.map(Chem.MolFromSmiles).map(bool)]
-    data = data[data.smiles.map(Chem.MolFromSmiles).map(Descriptors.ExactMolWt) < 600]
+    features = _physico_chemical_raw(data.smiles)  
+    valid_mask = (
+        ~torch.isnan(features).any(dim=1) &    # no NaNs
+        torch.isfinite(features).all(dim=1) &  # all finite
+        (features.abs() < 1e6).all(dim=1)      # within range
+    )
+    data = data[valid_mask.numpy()]
+    # remove all tiny tasks
     for task_id in data.task_id.unique():
         task = data[data.task_id == task_id]
         if len(task) < 10:
             data = data[data.task_id != task_id]
     data = data[data.label.astype(str).str.replace(".", "").str.isnumeric()]
     data.label = data.label.astype(float)
-    data = data.drop_duplicates().reset_index(drop=True)
+    data['inchi'] = data.smiles.map(Chem.MolFromSmiles).map(Chem.MolToInchi)
+    # remove all instances of mols, where the same mol appears multiple times in a dataset (task)
+    duplicates = data.duplicated(subset=['task_id', 'inchi'], keep=False)
+    data = data[~duplicates].reset_index(drop=True)
     return data
