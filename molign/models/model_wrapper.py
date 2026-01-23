4279de0b599ade8cd2378b7cff2b3873ed512ba7
Matthias Welsch <matthias.welsch@univie.ac.at>
2026-01-23 10:11:10 +0100

    Reduce load on memory

--- molign/models/model_wrapper.py
+++ molign/models/model_wrapper.py
@@ -1,10 +1,30 @@
-from typing import List
+from typing import List, Callable
 
 import numpy as np
 import torch
+
+from rdkit import Chem, RDLogger
+from rdkit.Chem import Descriptors, rdFingerprintGenerator
+from chembl_structure_pipeline import standardize_mol
+from sklearn.preprocessing import StandardScaler
+
+from rdkit.ML.Descriptors.MoleculeDescriptors import MolecularDescriptorCalculator
 from molfeat.trans.pretrained import PretrainedDGLTransformer
 from molfeat.trans.pretrained.hf_transformers import PretrainedHFTransformer
 from transformers import BertModel, BertTokenizer
+
+def neural_network(data: List[str], network: Callable):
+    batch_size = 500
+    res = []
+
+    for batch_id in range(0, len(data), batch_size):
+        batch = data[batch_id: batch_id + batch_size]
+        res.append(
+            torch.tensor(network(batch))
+        )
+
+    return torch.cat(res)
+
 
 def BERTModel(data: List[str], model_name: str, layer: int):
     tokenizer = BertTokenizer.from_pretrained(model_name)
@@ -23,17 +43,19 @@ def BERTModel(data: List[str], model_name: str, layer: int):
 
 
 def chemGPTs(data: List[str], model_name: str, layer: int):
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
     transformer = PretrainedHFTransformer(
-        kind=model_name, dtype=float, preload=True, concat_layers=layer
+        kind=model_name, dtype=float, preload=True, concat_layers=layer, device=device
     )
-    return torch.tensor(transformer(data))
+    return neural_network(data, transformer)
 
 
 def ChemBERT(data: List[str], model_name: str, layer: int):
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
     transformer = PretrainedHFTransformer(
-        kind=model_name, dtype=float, preload=True, concat_layers=layer
+        kind=model_name, dtype=float, preload=True, concat_layers=layer, device=device
     )
-    return torch.tensor(transformer(data))
+    return neural_network(data, transformer)
 
 
 def gins(data: List[str], model_name: str, layer: int):
@@ -41,3 +63,36 @@ def gins(data: List[str], model_name: str, layer: int):
     transformer.featurizer.num_layers = min(layer, transformer.featurizer.num_layers)
     transformer.featurizer.gnn_layers[layer - 1].activation = None
     return torch.tensor(transformer(data))
+
+def check_pc_validity(t: torch.Tensor, max_val: float = 1e6):
+    return bool(
+        not torch.isnan(t).any() and
+        torch.isfinite(t).all() and
+        (torch.abs(t) < max_val).all()
+    )
+
+def physico_chemical(data: List[str], model_name: str, layer: int):
+    features = _physico_chemical_raw(data)
+    scaler = StandardScaler()
+    return torch.tensor(scaler.fit_transform(features))
+
+def _physico_chemical_raw(data):
+    desc_list = [
+        desc_name
+        for desc_name, _ in Descriptors._descList
+        if desc_name not in ("Ipc", "BertzCT")
+    ]
+    descgen = MolecularDescriptorCalculator(desc_list)
+    features = np.stack(
+        [descgen.CalcDescriptors(standardize_mol(Chem.MolFromSmiles(smi))) for smi in data]
+    )
+    
+    return torch.tensor(features, dtype=torch.float64)
+
+def fingerprints(data: List[str], model_name: str, layer: int):
+    RDLogger.DisableLog("rdApp.*")
+    mfpgen = rdFingerprintGenerator.GetMorganGenerator(radius=2, fpSize=2048)
+    features = np.stack(
+        [np.array(mfpgen.GetFingerprint(standardize_mol(Chem.MolFromSmiles(smi)))) for smi in data]
+    )
+    return torch.tensor(features, dtype=torch.float64)
